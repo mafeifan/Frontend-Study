1236414d661cb54bcd78cfd37cf00d75f6c7ab5c
finley <mafeifan@qq.com>
2017-09-18 16:15:45 +0800

    node-express-2

--- Node/Express/index.js
+++ Node/Express/index.js
@@ -1,9 +1,20 @@
 var express = require("express");
-
+var logger = require("morgan");
 var app = express();
 
-app.get("/", function(request, response) { 
-  response.send("Hello world!");           
+// 一个流行的日志中间件
+// https://www.npmjs.com/package/morgan
+// 会输出类似 ::1 - GET /hello HTTP/1.1 - - 7.2 ms
+app.use(logger("short")); 
+// 简单的中间件
+app.use(function(request, response, next) {
+  console.log(request.ip)
+  console.log("In comes a " + request.method + " to " + request.url);
+  next();
+});
+
+app.use(function(request, response) { 
+  response.send("Hello world33!");           
 });                                  
 
 app.listen(3000, function() {                       
