352aa13fa35d4f9c81c4d6c6959d89a000f2c178
finley <mafeifan@qq.com>
2017-09-29 16:08:48 +0800

    react 基本实现

--- Javascript/React/my-app/src/Square.js
+++ Javascript/React/my-app/src/Square.js
@@ -1,13 +1,19 @@
 import React from 'react';
+// 下边的function Square()是简写
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {this.props.value}
-      </button>
-    );
-  }
+export default function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
-
-export default Square;
