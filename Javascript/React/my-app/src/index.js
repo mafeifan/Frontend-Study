eb156c8ee14b9bc90b9f316156fdfc9a98412884
finley <mafeifan@qq.com>
2017-09-29 14:45:00 +0800

    react 初始化棋盘

--- Javascript/React/my-app/src/index.js
+++ Javascript/React/my-app/src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Game from './Game';
+// 这个是啥
 import registerServiceWorker from './registerServiceWorker';
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('game'));
 registerServiceWorker();
