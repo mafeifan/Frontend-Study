0569b40ad951212b372f5c4d9f664708a6121c1c
finley <mafeifan@qq.com>
2017-10-04 22:41:31 +0800

    react 初步完成todolist react开发没vue快啊，不过更贴近原生

--- Javascript/React/my-app/.eslintrc.js
+++ Javascript/React/my-app/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
         // ReactDOM: false
     },
     rules: {
+      'no-trailing-spaces': 0,
         // 这里填入你的项目需要的个性化配置，比如：
         //
         // @fixable 一个缩进必须用两个空格替代
--- Javascript/React/my-app/src/ToDoList/index.js
+++ Javascript/React/my-app/src/ToDoList/index.js
@@ -1,21 +1,84 @@
 import React from 'react';
 
-// class ToDoList extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       lists: [
-//         { text: 'learn AngularJS', done: true },
-//         { text: 'build an AngularJS app', done: false }
-//       ]
-//     };
-//   }
-// }
+class TypeNew extends React.Component {
+  handleAdd = (e) => {
+    e.preventDefault();
+    const newInput = this._inputElement.value.trim();
+    let items = this.props.items;
+    if (newInput !== '') {
+      items.push({
+        text: newInput,
+        done: false
+      });
+      this.props.onAdd(items);
+    }
+    this._inputElement.value = '';
+    console.dir(this);
+  }
 
-
-
-export default function ToDoList(props) {
-  const lists = props.lists;
-  const listsItems = lists.map(item => <li key={item.text}>{item.text}</li>);
-  return (<ul>{listsItems}</ul>);
+  render() {
+    return (
+      <form onSubmit={this.handleAdd}>
+        <input type="text" ref={(a) => this._inputElement = a} placeholder="search.." />
+      </form>
+    );
+  }
 }
+
+class ToDoItems extends React.Component {
+  handleDel = (e) => {
+    const delIndex = e.target.getAttribute('data-key');
+    this.props.items.splice(delIndex, 1);
+    this.props.onDel(this.props.items);
+  }
+  render() {
+    return (<ul>
+      {this.props.items.map((item, i) =>
+        <li key={item.text}>
+          <label>{item.text}</label>
+          <button onClick={this.handleDel} data-key={i}>delete</button>
+        </li>
+      )}
+    </ul>);
+  }
+}
+
+
+export default class ToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { text: 'learn AngularJS', done: true },
+        { text: 'build an AngularJS app', done: false }
+      ]
+    };
+    // 这行非常重要
+    // https://www.smashingmagazine.com/2014/01/understanding-javascript-function-prototype-bind/
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(item) {
+    this.setState({
+      items: item
+    })
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <TypeNew onAdd={this.handleChange} items={this.state.items} />
+        <ToDoItems onDel={this.handleChange} items={this.state.items} />
+      </div>
+    );
+  }
+}
+
+// export default function ToDoList(props) {
+//   const lists = props.lists;
+//   const listsItems = lists.map(item => <li key={item.text}>
+//     <input type="checkbox" checked={item.done} />{item.text}</li>);
+//   return (<ul>{listsItems}</ul>);
+// }
