0be0eb611568e4cd406c264e8f1b947f8488927d
finley <mafeifan@qq.com>
2017-10-08 19:30:12 +0800

    update

--- Javascript/React/my-app/src/Comment/index.js
+++ Javascript/React/my-app/src/Comment/index.js
@@ -1,92 +1,136 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class CommentForm extends React.Component {
-  handleAdd = (e) => {
-    e.preventDefault();
-    const newInput = this._inputElement.value.trim();
-    const newTextArea = this._textareaElement.value.trim();
-    let items = this.props.items;
-    if (newInput !== '' && newTextArea !== '') {
-      items.push({
-        name: newInput,
-        content: newTextArea
-      });
-      this.props.onAdd(items);
-    }
-    this._textareaElement.value = '';
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleAdd}>
-        <div>
-          <label>用户名</label>
-          <input type="text" ref={(a) => this._inputElement = a} />
-        </div>
-        <div>
-          <label>内容</label>
-          <textarea ref={(a) => this._textareaElement = a} name="content" id="" cols="30" rows="10"></textarea>
-        </div>
-        <div><button>发布</button></div>
-      </form>
-    );
-  }
-}
-
-class CommentList extends React.Component {
-  render() {
-    const items = this.props.items;
-    return items.map((item, id) => <Comment comment={item} key={id}></Comment>);
-  }
-}
-
-class Comment extends React.Component {
-  // https://github.com/facebook/prop-types
-  static propTypes = {
-    comment: PropTypes.object.isRequired
-  }
-  render() {
-    const { comment } = this.props.comment;
-    return (
-      <div style={{ border: '1px solid black' }}>
-        <span>{comment.name}</span>
-        <p>{comment.content}</p>
-      </div>
-    );
-  }
-}
-
 export default class CommentApp extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      items: []
+      comments: []
     };
-    // 这行非常重要
-    // https://www.smashingmagazine.com/2014/01/understanding-javascript-function-prototype-bind/
-    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(item) {
+  handleSubmitComment(comment) {
+    if (!comment) return;
+    if (!comment.username) return alert('请输入用户名');
+    if (!comment.content) return alert('请输入评论内容');
+    this.state.comments.push(comment);
     this.setState({
-      items: item
+      comments: this.state.comments
     });
   }
 
   render() {
     return (
-      <div>
-        <CommentForm onAdd={this.handleChange} items={this.state.items}></CommentForm>
-        <CommentList items={this.state.items}></CommentList>
+      <div className="wrapper">
+        <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
+        <CommentList comments={this.state.comments} />
       </div>
     );
   }
 }
 
-// export default function ToDoList(props) {
-//   const lists = props.lists;
-//   const listsItems = lists.map(item => <li key={item.text}>
-//     <input type="checkbox" checked={item.done} />{item.text}</li>);
-//   return (<ul>{listsItems}</ul>);
-// }
+class CommentInput extends React.Component {
+  static propTypes = {
+    onSubmit: PropTypes.func
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      content: ''
+    };
+  }
+
+  componentDidMount() {
+    this.textarea.focus();
+  }
+
+  _saveUsername(username) {
+    localStorage.setItem('username', username);
+  }
+
+  handleUsernameChange(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  handleContentChange(event) {
+    this.setState({
+      content: event.target.value
+    });
+  }
+
+  handleSubmit() {
+    if (this.props.onSubmit) {
+      const { username, content } = this.state;
+      this.props.onSubmit({ username, content });
+    }
+    this.setState({ content: '' });
+  }
+
+  handleUsernameBlur(event) {
+    this._saveUsername(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="comment-input">
+        <div className="comment-field">
+          <span className="comment-field-name">用户名：</span>
+          <div className="comment-field-input">
+            <input
+              value={this.state.username}
+              onChange={this.handleUsernameChange.bind(this)}
+              onBlur={this.handleUsernameBlur.bind(this)}
+            />
+          </div>
+        </div>
+        <div className="comment-field">
+          <span className="comment-field-name">评论内容：</span>
+          <div className="comment-field-input">
+            <textarea
+              value={this.state.content}
+              ref={a => (this.textarea = a)}
+              onChange={this.handleContentChange.bind(this)}
+            />
+          </div>
+        </div>
+        <div className="comment-field-button">
+          <button onClick={this.handleSubmit.bind(this)}>发布</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Comment extends React.Component {
+  render() {
+    return (
+      <div className="comment">
+        <div className="comment-user">
+          <span>{this.props.comment.username} </span>：
+        </div>
+        <p dangerouslySetInnerHTML={{ __html: this.props.comment.content }} />
+      </div>
+    );
+  }
+}
+
+class CommentList extends React.Component {
+  static defaultProps = {
+    comments: []
+  };
+  render() {
+    return (
+      <div>
+        {this.props.comments.map((comment, i) => (
+          <Comment comment={comment} key={i} />
+        ))}
+      </div>
+    );
+  }
+}
+
+
