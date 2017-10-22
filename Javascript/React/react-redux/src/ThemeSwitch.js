3a3a22937194bac454c246f7ebdbf53aa64bf5fb
finley <mafeifan@qq.com>
2017-10-22 22:06:03 +0800

    udpate redux

--- Javascript/React/react-redux/src/ThemeSwitch.js
+++ Javascript/React/react-redux/src/ThemeSwitch.js
@@ -1,14 +1,48 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from './react-redux'
 
 class ThemeSwitch extends Component {
+  static contextTypes = {
+    themeColor: PropTypes.string,
+    onSwitchColor: PropTypes.func
+  }
+
+  // dispatch action 去改变颜色
+  handleSwitchColor (color) {
+    if (this.props.onSwitchColor) {
+      this.props.onSwitchColor(color)
+    }
+  }
+
   render () {
     return (
       <div>
-        <button>Red</button>
-        <button>Blue</button>
+        <button
+          style={{ color: this.props.themeColor }}
+          onClick={this.handleSwitchColor.bind(this, 'red')}>Red</button>
+        <button
+          style={{ color: this.props.themeColor }}
+          onClick={this.handleSwitchColor.bind(this, 'blue')}>Blue</button>
       </div>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSwitchColor: (color) => {
+      dispatch({ type: 'CHANGE_COLOR', themeColor: color })
+    }
+  }
+}
+
+ThemeSwitch = connect(mapStateToProps, mapDispatchToProps)(ThemeSwitch)
 
 export default ThemeSwitch
--- Javascript/React/react-redux/src/index.js
+++ Javascript/React/react-redux/src/index.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom'
 import Header from './Header'
 import Content from './Content'
+import Demo from './Demo'
 import './index.css'
 
+const themeReducer = (state, action) => {
+  if (!state) return {
+    themeColor: 'red'
+  }
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, themeColor: action.themeColor }
+    default:
+      return state
+  }
+}
+
+const store = createStore(themeReducer)
+
 class Index extends Component {
+  static childContextTypes = {
+    store: PropTypes.object
+  }
+
+  getChildContext () {
+    return { store }
+  }
+
   render () {
     return (
       <div>
         <Header />
         <Content />
+        <Demo />
       </div>
     )
   }
 }
 
 ReactDOM.render(
-  <Index />,
+  <Provider store={store}>
+    <Index />
+  </Provider>,
   document.getElementById('root')
 )
+
