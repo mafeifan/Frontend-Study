40c5a360cdd7af35a6b2ce68db2e9e57114a60e1
finley <mafeifan@qq.com>
2017-12-21 19:11:43 +0800

    Chrome/Extensions/mayiwo/index.html

--- Chrome/Extensions/mayiwo/index.js
+++ Chrome/Extensions/mayiwo/index.js
@@ -1,11 +1,22 @@
 // 1. 获取mp3地址
 // 2. 利用jsmediatags 获取mp3信息
 
-jsmediatags.read("http://mp3file1.mafengwo.net/201712221339/4717ea67076f6d094ef847fd29cbc1ac/s10/M00/2E/E1/wKgBZ1njcHWAEDioACmXUq2G7RQ744.mp3", {
-  onSuccess: function(tag) {
-    console.log(tag);
-  },
-  onError: function(error) {
-    console.log(error);
+document.addEventListener('DOMContentLoaded', function () {
+
+  const MP3_URL = ''
+  if (document.querySelector(".hd_audio")) {
+    MP3_URL = document.querySelector(".hd_audio").getAttribute('data-music')
+    console.log('read')
+    jsmediatags.read(MP3_URL, {
+      onSuccess: function(tag) {
+        console.log(tag);
+      },
+      onError: function(error) {
+        console.log(error);
+      }
+    });
   }
+
+  console.log('end')
 });
+
