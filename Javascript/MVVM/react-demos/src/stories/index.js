3b7aae1d679d1d53c4b39af5dc783c1c559326dc
finley <mafeifan@qq.com>
2017-12-31 12:45:51 +0800

    stories 加注释

--- Javascript/MVVM/react-demos/src/stories/index.js
+++ Javascript/MVVM/react-demos/src/stories/index.js
@@ -6,8 +6,12 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
+// 文档 https://storybook.js.org/basics/writing-stories/
+// storiesOf应该是分组，每组添加一个个story
+// 修改内容页面会实时发生变化
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
+// 使用action让storybook去记录log,可以在页面的action logger中查看
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
