6a47de472c9d61edbd29c51c885f769987c00c22
finley <mafeifan@qq.com>
2018-01-01 17:19:14 +0800

    Vue demo for chrome Dynamic-module-imports

---
+++ Chrome/63/Dynamic-module-imports/vue-demo/pages/GamesPage.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'GamesPage',
+  template: `
+    <div>
+     <h1>Games Page</h1>
+     <p>{{ message }}</p>
+    </div>
+  `,
+  data() {
+    return {
+      message: 'Oh hai from the games page'
+    }
+  }
+};
