e12466053ed88f9df1acab5a67f393d997e07500
Finley Ma <mafeifan@qq.com>
2021-06-29 17:36:59 +0800

    mqtt part

--- mqtt/3.websocket/publish.js
+++ mqtt/3.websocket/publish.js
@@ -5,6 +5,7 @@ client.on('connect', () => {
   // 每隔 1s 发布主题为 presence 的消息
   setInterval(function () {
     let message = new Date().toLocaleString()
+    console.log(`发送消息${message}`)
     client.publish('presence', message, {
       qos: 0,         // 至多一次 可以丢失
       retain: false   // 不保留
--- mqtt/3.websocket/server.js
+++ mqtt/3.websocket/server.js
@@ -1,12 +1,23 @@
 const aedes = require('aedes')()
 const httpServer = require('http').createServer()
 const ws = require('websocket-stream')
-const port = 8888
+const express = require('express')
+const app = express()
 
-// 连接地址
+const httpPort = 8080
+const wsPort = 8888
+
+// 启动本地HTTP服务器，用于打开静态页面 http://localhost:8008
+app.use(express.static(__dirname + '/public'))
+app.listen(httpPort, () => {
+  console.log(`Example app listening at http://localhost:${httpPort}`)
+})
+
+
+// 启动一个websocket服务端
 // ws://localhost:8888/mqtt
 ws.createServer({ server: httpServer }, aedes.handle)
 
-httpServer.listen(port, function () {
-  console.log('websocket server listening on port ', port)
+httpServer.listen(wsPort, function () {
+  console.log(`WebSocket server listening at ws://localhost:${wsPort}/mqtt`)
 })
